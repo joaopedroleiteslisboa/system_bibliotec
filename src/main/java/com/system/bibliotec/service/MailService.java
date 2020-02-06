0cc5e11eb8382e94f135796cce1a73e27f5b7b62
João Pedro Leite S Lisboa <joaopedroleite.s.lisboa.info@gmail.com>
2020-02-06 17:41:41 -0300

    .....

---
+++ src/main/java/com/system/bibliotec/service/MailService.java
@@ -0,0 +1,13 @@
+package com.system.bibliotec.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MailService {
+
+	
+	private final Logger log = LoggerFactory.getLogger(MailService.class);
+}
